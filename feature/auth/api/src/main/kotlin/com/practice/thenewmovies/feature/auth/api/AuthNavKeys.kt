c2e93f406e3216efb78dda25f92831662e3544aa
danhtruong254 <danhtruong@everfit.io>
2026-08-22 13:19:42 +0700

    feat(auth): add the auth navigation keys

---
+++ feature/auth/api/src/main/kotlin/com/practice/thenewmovies/feature/auth/api/AuthNavKeys.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 TheNewMovies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.practice.thenewmovies.feature.auth.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object LoginNavKey : NavKey
+
+@Serializable
+data object SignUpNavKey : NavKey
+
+@Serializable
+data object ForgotPasswordNavKey : NavKey
+
+@Serializable
+data class ResetPasswordNavKey(val email: String) : NavKey
